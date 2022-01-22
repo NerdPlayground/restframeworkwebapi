b9647cdca1840510fba35bf54cfe94b50689e2f8
Paqimm <mcgeewilliam815@gmail.com>
2022-01-22 13:37:47 +0300

    chore (Model) : create Snippets model

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,25 @@
+from sre_constants import CH_LOCALE
+from statistics import mode
+from turtle import update
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
-# Create your models here.
+LEXERS= [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES= sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES= sorted([(item,item) for item in get_all_styles()])
+
+# model == relation
+class Snippet(models.Model):
+    # set the time of creation and updation
+    created_at= models.DateTimeField(auto_now_add=True)
+    update_at= models.DateTimeField(auto_now=True)
+    
+    title= models.CharField(max_length=100,blank=True,default='')
+    code= models.TextField()
+    linenos= models.BooleanField(default=False)
+    language= models.CharField(choices=LANGUAGE_CHOICES,default='python',max_length=100)
+    style= models.CharField(choices=STYLE_CHOICES,default='friendly',max_length=100)
+
+    class Meta:
+        ordering= ['created_at']
