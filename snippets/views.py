eb1cf75a750a68406287026cfb0ce4df0e1a292b
Paqimm <mcgeewilliam815@gmail.com>
2022-01-24 13:12:28 +0300

    refractor (Views) : Multiple changes Use function based views & Response objects Add format suffixes to the URLS (Response doesn't stick to one format User can refer to any specific format) Add status codes

--- snippets/views.py
+++ snippets/views.py
@@ -1,12 +1,55 @@
-from ast import Pass
-from http.client import HTTPResponse
+from rest_framework import status
+from snippets.models import Snippet
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from snippets.serializers import SnippetSerializer
+
+@api_view(['GET','POST'])
+def snippet_list(request,format=None):
+    if request.method == 'GET':
+        snippets= Snippet.objects.all()
+        serializer= SnippetSerializer(snippets,many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer= SnippetSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def snippet_detail(request,pk,format=None):
+    try:
+        snippet= Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer= SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer= SnippetSerializer(snippet,data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+'''
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from snippets.models import Snippet
+from rest_framework.decorators import api_view
 from snippets.serializers import SnippetSerializer
 
 # clients won't have csrf token
+# The server won't check for the token
+# The request won't be rejected
+
 @csrf_exempt
 def snippet_list(request):
     if request.method == 'GET':
@@ -27,7 +70,7 @@ def snippet_detail(request, pk):
     try:
         snippet= Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HTTPResponse(status=404)
+        return HttpResponse(status=404)
     
     if request.method == 'GET':
         serializer= SnippetSerializer(snippet)
@@ -43,4 +86,5 @@ def snippet_detail(request, pk):
     
     elif request.method == 'DELETE':
         snippet.delete()
-        return HTTPResponse(status=204)
+        return HttpResponse(status=204)
+'''
