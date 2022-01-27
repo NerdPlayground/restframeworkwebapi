d064c9e0404d50f3ea854985e0528c33237a91b0
Paqimm <mcgeewilliam815@gmail.com>
2022-01-27 12:36:25 +0300

    fix (NOT NULL) : add user to the post method

--- snippets/views.py
+++ snippets/views.py
@@ -66,13 +66,13 @@ class SnippetAPIView(APIView):
         # serializer.is_valid(raise_exception=True)
         # return Response(serializer.data,status=status.HTTP_200_OK)
         if serializer.is_valid() == True:
-            serializer.save()
+            serializer.save(owner=self.request.user)
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 class SnippetDetailAPIView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
-    
+
     def get_object(self, pk):
         try:
             return Snippet.objects.get(pk=pk)
