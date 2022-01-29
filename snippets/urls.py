adf0af3e08e33ffd5bd2810c86d9274f9e40af2e
Paqimm <mcgeewilliam815@gmail.com>
2022-01-29 13:54:53 +0300

    refractor (Views,URLs) : use viewsets and routers

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,3 +1,56 @@
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from snippets.views import SnippetViewSet,UserViewSet
+
+router= DefaultRouter()
+router.register(r'users',UserViewSet)
+router.register(r'snippets',SnippetViewSet)
+
+urlpatterns=[
+    path('',include(router.urls))
+]
+
+'''
+# ===> Explicit URL to View Binding <===
+from django.urls import path
+from rest_framework import renderers
+from snippets.views import api_root,UserViewSet,SnippetViewSet
+
+snippet_list= SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+snippet_detail= SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+snippet_highlight= SnippetViewSet.as_view({
+    'get': 'highlight'
+},renderer_classes= [renderers.StaticHTMLRenderer])
+
+user_list= UserViewSet.as_view({
+    'get': 'list'
+})
+
+user_detail= UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+urlpatterns = [
+    path('',api_root),
+    path('users/',user_list,name='user-list'),
+    path('users/<int:pk>/',user_detail,name='user-detail'),
+    path('snippets/',snippet_list,name='snippet-list'),
+    path('snippets/<int:pk>/',snippet_detail,name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',snippet_highlight,name='snippet-highlight'),
+]
+'''
+
+'''
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets.views import api_root,SnippetAPIView,SnippetDetailAPIView,UserAPIView,UserDetailAPIView,SnippetHighlightAPIView
@@ -12,6 +65,7 @@ urlpatterns = [
 ]
 
 urlpatterns= format_suffix_patterns(urlpatterns)
+'''
 
 '''
 # ===> FUNCTION BASED VIEWS <===
--- snippets/views.py
+++ snippets/views.py
@@ -5,27 +5,39 @@ from rest_framework import renderers
 from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 from rest_framework.response import Response
-from rest_framework.decorators import APIView
-from rest_framework.decorators import api_view
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.generics import GenericAPIView
+from rest_framework.decorators import APIView,api_view,action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from snippets.serializers import SnippetSerializer,UserSerializer
+from rest_framework.viewsets import ReadOnlyModelViewSet,ModelViewSet
 
+
+class UserViewSet(ReadOnlyModelViewSet):
+    queryset= User.objects.all()
+    serializer_class= UserSerializer
+
+class SnippetViewSet(ModelViewSet):
+    queryset= Snippet.objects.all()
+    serializer_class= SnippetSerializer
+    permission_classes=[IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+
+    @action(detail=True,renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet= self.get_object()
+        return Response(snippet.highlighted)
+    
+    def perform_create(self,serializer):
+        serializer.save(owner=self.request.user)
+
+'''
+# ===> View Classes <====
 @api_view(['Get'])
 def api_root(request,format=None):
     return Response({
         'users':reverse('user-list',request=request,format=format),
         'snippets':reverse('snippet-list',request=request,format=format),
     })
-
-class SnippetHighlightAPIView(GenericAPIView):
-    queryset= Snippet.objects.all()
-    renderer_classes= [renderers.StaticHTMLRenderer]
-
-    def get(self, request, *args, **kwargs):
-        snippet= self.get_object()
-        return Response(snippet.highlighted)
 
 class UserAPIView(APIView):
 
@@ -124,6 +136,15 @@ class SnippetDetailAPIView(APIView):
         snippet= self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class SnippetHighlightAPIView(GenericAPIView):
+    queryset= Snippet.objects.all()
+    renderer_classes= [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet= self.get_object()
+        return Response(snippet.highlighted)
+'''
 
 '''
 # ===> Generic Class Based Views <===
